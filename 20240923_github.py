85f39358d54958e7b712e50e4c3cbd4b2709f9ac
Kiwirice <67418@sfbbildungszentrum.onmicrosoft.com>
2024-09-23 11:10:16 +0200

    Anzeige hinzf

--- 20240923_github.py
+++ 20240923_github.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 st.title("Counter")
 
-
 if 'number' not in st.session_state:
     st.session_state['number'] = 0
 
@@ -15,5 +14,39 @@ if st.button('Down'):
 if st.button("Reset"):
     st.session_state['number'] = 0
     
-    
 st.write(st.session_state.number)
+
+a = (".\GP_2\Download_1.jpg")
+b = (".\GP_2\Download_2.jpg")
+c = (".\GP_2\Download_3.jpg")
+d = (".\GP_2\Screenshot 2024-09-09 082910.png")
+e = (".\GP_2\Screenshot 2024-09-09 103707.png")
+f = (".\GP_2\Screenshot 2024-09-17 190904.png")
+
+col1, col2, col3 = st.columns (3)
+col4, col5, col6 = st.columns (3)
+
+
+with col1:
+    st.header("Bild 1")
+    st.image(a)
+
+with col2:
+    st.header("Bild 2")
+    st.image(b)
+
+with col3:
+    st.header("Bild 3")
+    st.image(c)
+
+with col4:
+    st.header("Bild 4")
+    st.image(d)
+
+with col5:
+    st.header("Bild 5")
+    st.image(e)
+
+with col6:
+    st.header("Bild 6")
+    st.image(f)
